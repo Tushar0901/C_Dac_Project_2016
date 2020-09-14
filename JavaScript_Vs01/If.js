3791b440b426573defbd377e1e060ec76ab031d7
Tushar Bhavsar <71285178+Tushar0901@users.noreply.github.com>
2020-09-14 15:10:14 -0400

    Add files via upload

---
+++ JavaScript_Vs01/If.js
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>New Page 1</title>
+</head>
+
+<body>
+
+
+if(15>0)
+{
+document.write("addition is")
+}
+
+</body>
+
+</html>
---
+++ JavaScript_Vs01/else if.js
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>New Page 1</title>
+</head>
+
+<body>
+<script language=javascript>
+var a
+a=eval(prompt("enter value"))
+
+if(a>0)
+{
+document.write("pos")
+}
+
+else if(a<0)
+{
+document.write("neg")
+}
+
+if
+{
+document.write("zero")
+}
+
+</script>
+
+</body>
+
+</html>
